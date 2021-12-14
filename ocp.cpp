48a1c79f65b9a323ffa164f006c285f38c060c97
mechaniccoder <yuseunghwan94@gmail.com>
2021-12-14 22:35:18 +0900

    OCP - boilerplate

---
+++ ocp.cpp
@@ -0,0 +1,12 @@
+class Square {
+
+}
+
+class Renderer {
+    public:
+        void render(const vector<Square>& squares);
+}
+
+void Renderer::render(const vector<Square>& squares) {
+    for (auto& square : squares) {}
+}
